e9f6491bc28ee455f031ef91ce63fc89e7ea71f3
Adler <adleralves@gamil.com>
2026-04-05 12:51:03 -0300

    Adicionei o beforeEach para acesso a página de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,16 @@
 describe('Login', () => {
-  it('Login com dados válidos deve permitir entrada no sistema', () => {
+
+  beforeEach(() => {
     // Arrange
     cy.visit('http://localhost:4000')
+  })
+  it('Login com dados válidos deve permitir entrada no sistema', () => {
 
     // Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
     //cy.get('#login-section > .btn').click()
-    cy.contains('button','Entrar').click()
+    cy.contains('button', 'Entrar').click()
 
     // Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
@@ -15,16 +18,14 @@ describe('Login', () => {
 
   //it.only('Login com dados inválidos deve apresentar mensagem de erro', () => {
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
-    // Arrange
-    cy.visit('http://localhost:4000')
 
     // Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('654321')
     //cy.get('#login-section > .btn').click()
-    cy.contains('button','Entrar').click()
+    cy.contains('button', 'Entrar').click()
 
     // Assert
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
-  })  
+  })
 })
