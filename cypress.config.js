299b6a7c1ad19859297b2e3b65ae300ee1cadbbf
Adler <adleralves@gamil.com>
2026-04-14 20:30:30 -0300

    Adicioando o relatório HTML do Mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
     baseUrl: 'http://localhost:4000',
-    video: false,
+    reporter: 'cypress-mochawesome-reporter',
+    // video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 });
+
+
+
